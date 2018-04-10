a1dc5956ce5b4b9f6bd5cfa3a38257d7e7ec4a21
Andrew Frost <desertghost@nevada.unr.edu>
2018-04-09 18:20:52 -0700

    Add Game Backend (#2)
    * Implement Board Data class. Create Game Controller class.
    
    * Implement Game Controller and Board Data classes (#1)
    
    * Created a test scene.
    
    * Implemented all board to string functions.
    
    * Implement Game Controller.
    
    * implement test + bug fixes

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,185 @@
+﻿/*
+ * 
+ * Author: Andrew Frost
+ * 
+ * Copy Right (c) 2018 All Rights Reserved
+ * 
+ * 4/9/2018
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+//runs the game, determines winner, etc
+public class GameController : MonoBehaviour
+{
+    public Vector3Int boardSize = new Vector3Int(4, 4, 4);
+
+    [SerializeField]
+    private PlayerEvent winEvent = null;
+
+    [SerializeField]
+    private UnityEvent yellowPlayerTurn = null;
+
+    [SerializeField]
+    private UnityEvent redPlayerTurn = null;    
+
+    private bool gameOver = false;
+
+    [SerializeField]
+    private Token currentPlayer = Token.yellow;
+
+    private Token winner = Token.empty;
+
+    private BoardData board;
+    
+	void Awake ()
+    {
+        gameOver = false;
+        currentPlayer = Token.yellow;
+        board = new BoardData(boardSize);
+        boardSize = board.Size;
+	}
+
+    public void PlaceToken(Vector3Int coordinate, Token token)
+    {
+        if (!gameOver 
+            && token == currentPlayer 
+            && board.TrySetValue(coordinate, token))
+        {
+            if (IsGameOver())
+            {
+                //set whichever player won as winner
+                SetWinner();
+            }
+            else
+            {
+                //switch player
+                ChangeTurn();
+            }            
+        }        
+    }
+
+    //check if the game has been won. Sets winner to the winning player if a winner exists
+    public bool IsGameOver()
+    {
+        string test;
+        Vector3Int position = Vector3Int.zero;
+
+        test = "";
+
+        // build test data 
+        test = board.GetXYZBoard()
+            + board.GetXZYBoard()
+            + board.GetYXZBoard()
+            + board.GetYZXBoard()
+            + board.GetZXYBoard()
+            + board.GetZYXBoard()
+            + board.GetDxDyDzBoard()
+            + board.GetDxDyInverseDzBoard()
+            + board.GetDxInverseDyDzBoard()
+            + board.GetDxInverseDyInverseDzBoard()
+            + board.GetXDyDzBoard()
+            + board.GetXDyDzInverseBoard()
+            + board.GetYDxDzBoard()
+            + board.GetYDxDzInverseBoard()
+            + board.GetZDxDyBoard()
+            + board.GetZDxDyInverseBoard();
+
+        //check for yellow win
+        if (IsWinner(Token.yellow, test))
+        {
+            winner = Token.yellow;
+
+            return true;
+        }
+
+        //check for red win
+        if (IsWinner(Token.red, test))
+        {
+            winner = Token.red;
+
+            return true;
+        }
+
+        //check for draw
+        if (!test.Contains(Token.empty.ToString()))
+        {
+            winner = Token.empty;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public string Print()
+    {
+        return board.ToString();
+    }
+
+    private bool IsWinner(Token token, string test)
+    {
+        if (test.Contains(GetKey(token)))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private string GetKey(Token token)
+    {
+        int index;
+        string key = "";
+
+        for (index = 0; index < 4; index++)
+        {
+            key += token.ToString() + " ";
+        }
+
+        return key;
+    }
+
+    private void SetWinner()
+    {
+        gameOver = true;
+
+        if (winEvent != null)
+        {
+            winEvent.Invoke(winner);
+        }
+        else
+        {
+            Debug.LogError("Cannot invoke win event: event is null!");
+        }
+    }
+
+    private void ChangeTurn()
+    {
+        currentPlayer = (currentPlayer == Token.yellow ? Token.red : Token.yellow);
+
+        InvokeTurnChange((currentPlayer == Token.yellow ? yellowPlayerTurn : redPlayerTurn), currentPlayer);
+    }
+
+    private static void InvokeTurnChange(UnityEvent callback, Token playerID)
+    {
+        if (callback != null)
+        {
+            callback.Invoke();
+        }
+        else
+        {
+            Debug.LogError("Cannot pass game to " + playerID.ToString() + " player: event is null!");
+        }
+    }
+}
+
+[System.Serializable]
+public class PlayerEvent : UnityEvent<Token> { }
+
+[System.Serializable]
+public class TokenEvent : UnityEvent<Vector3Int, Token> { }
---
+++ Assets/Scripts/GameTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTest : MonoBehaviour
+{
+
+    public Vector3Int position = Vector3Int.zero;
+
+    private GameController gameController;
+
+    private Token token;
+
+	// Use this for initialization
+	void Start ()
+    {
+        gameController = GetComponent<GameController>();
+        token = Token.yellow;
+
+        Debug.Log(gameController.Print());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gameController.PlaceToken(position, token);
+
+            Debug.Log(gameController.Print());
+        }
+	}
+
+    public void SetYellow()
+    {
+        token = Token.yellow;
+    }
+    
+    public void SetRed()
+    {
+        token = Token.red;
+    }
+
+    public void PrintWinner(Token token)
+    {
+        Debug.Log(token.ToString() + " won!");
+    }
+}
