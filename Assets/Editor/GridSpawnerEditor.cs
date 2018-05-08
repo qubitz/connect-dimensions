e23e1888806de4eaef1080310d5f62ab9798c727
Tyler Goffinet <ternarytyler@gmail.com>
2018-05-08 01:40:58 -0700

    Dynamically create user board
    * Spawns board made of snap drop zones
    * Board can be any size
    * Board can be picked up and dropped
    * Board can be set to be auto grabbed on start
    * Made token spheres
    * Added scripts to `Main` scene

---
+++ Assets/Editor/GridSpawnerEditor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GridSpawner))]
+public class GridSpawnerEditor : Editor
+{
+    Vector3 spawnPosition = Vector3.zero;
+    Vector3Int sizes = new Vector3Int(4, 4, 4);
+    string prefabName = string.Empty;
+
+    public override void OnInspectorGUI()
+    {
+        spawnPosition = EditorGUILayout.Vector3Field("Spawn Position", spawnPosition);
+        sizes = EditorGUILayout.Vector3IntField("Grid Sizes", sizes);
+        prefabName = EditorGUILayout.TextField("Prefab Name", prefabName);
+
+        if (GUILayout.Button("Construct Board"))
+        {
+            var prefab = GetPrefabFromKeyword(prefabName);
+            var test = new GameObject("Board Test");
+            test.transform.position = spawnPosition;
+            GridSpawner.ConstructPlanes(sizes, test, prefab);
+        }
+        if (GUILayout.Button("Construct Plane"))
+        {
+            var prefab = GetPrefabFromKeyword(prefabName);
+            var test = new GameObject("Plane Test");
+            test.transform.position = spawnPosition;
+            GridSpawner.ConstructPlane(new Vector2Int(sizes.x, sizes.y), test, prefab);
+        }
+        if (GUILayout.Button("Construct Column"))
+        {
+            var prefab = GetPrefabFromKeyword(prefabName);
+            var test = new GameObject("Column Test");
+            test.transform.position = spawnPosition;
+            GridSpawner.ConstructColumn(sizes.y, test, prefab);
+        }
+    }
+
+    private GameObject GetPrefabFromKeyword(string keyword)
+    {
+        var prefabGUID = AssetDatabase.FindAssets(keyword);
+        var prefabPath = AssetDatabase.GUIDToAssetPath(prefabGUID[0]);
+        return (GameObject) AssetDatabase.LoadAssetAtPath(prefabPath, typeof(GameObject));
+    }
+}
---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public static class Extensions
+{
+    public static void SetParent(this GameObject go, GameObject newParent)
+    {
+        go.transform.parent = newParent.transform;
+    }
+
+    public static void SetParent(this GameObject go, Transform newParent)
+    {
+        go.transform.parent = newParent;
+    }
+
+    public static void SetParentRelative(this GameObject go, GameObject newParent)
+    {
+        var newPos = go.transform.localPosition + newParent.transform.localPosition;
+        var savedScale = go.transform.localScale;
+        go.transform.parent = newParent.transform;
+        go.transform.localPosition = newPos;
+        go.transform.localScale = savedScale;
+    }
+
+    public static void SetParentRelative(this GameObject go, Transform newParent)
+    {
+        go.transform.position += newParent.position;
+        var savedScale = go.transform.localScale;
+        go.transform.parent = newParent;
+        go.transform.localScale = savedScale;
+    }
+}
